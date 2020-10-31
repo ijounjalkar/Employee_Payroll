34261bc48cbb3d25fc9ea23609c9f7eed35e843d
ijounjalkar <jounjalkarisha@gmail.com>
2020-10-31 20:20:01 +0530

    Added functionality to extend the database

--- Emp_Payroll.sql
+++ Emp_Payroll.sql
@@ -55,4 +55,19 @@ select gender, max(salary) from employee_payroll group by gender;
 select count(*) from employee_payroll;
 select gender, count(*) from employee_payroll group by gender;
 
+//UC8
+
+alter table employee_payroll_service add phone numeric(10) after name;
+alter table employee_payroll_service add address varchar(250) after phone;
+alter table employee_payroll_service add department varchar(150) NOT NULL after address;
+alter table employee_payroll_service alter address SET DEFAULT 'TBD';
+
+//UC9
+
+alter table employee_payroll_service RENAME COLUMN salary to basic_pay;
+alter table employee_payroll_service add deductions double NOT NULL after basic_pay;
+alter table employee_payroll_service add taxable_pay double NOT NULL after deductions;
+alter table employee_payroll_service add tax double NOT NULL after taxable_pay;
+alter table employee_payroll_service add net_pay double NOT NULL after tax;
+
 
